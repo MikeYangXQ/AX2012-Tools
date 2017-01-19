910ef967fd56c94f1ff50f8db00445a07bb15bcf
José Antonio Estevan <jaestevan@gmail.com>
2017-01-19 19:59:11 +0100

    Revert "Current version of the AX 2012 X++ Editor Extensions"

--- MorphX IDE/Editor Extensions/JAEEHighlightWordExtension/HighlightWordTag.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.VisualStudio.Text.Tagging;
-
-namespace JAEE.AX.EditorExtensions
-{
-    internal class HighlightWordTag : TextMarkerTag
-    {
-        public HighlightWordTag() : base("MarkerFormatDefinition/HighlightWordFormatDefinition") { }
-    }
-}
